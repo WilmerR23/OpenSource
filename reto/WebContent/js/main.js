b51ea26f5a184d0df7c59337fa98328de99cc74f
Wilmer <kwilram23@gmail.com>
2019-11-04 12:17:07 -0400

    Tercer commit con todo implementado solo falta algunas validaciones

--- reto/WebContent/js/main.js
+++ reto/WebContent/js/main.js
@@ -14,7 +14,13 @@ var app = new Vue({
         map: null,
         markersArray: [],
         latitude: null,
-        longitude: null
+        longitude: null,
+        secondMap: null,
+        markersSecondMapArray: [],
+        markersInfoWindows: [],
+        currentClicked: null,
+        directionsService: null,
+        directionsRenderer: null        
     },
     mounted () {
     	this.initMap();
@@ -26,11 +32,10 @@ var app = new Vue({
     },
     methods: {
         fetch() {
-            this.axs.get('/personas')
-            .then(
-                response => (
+            this.axs.get('/personas').then(response => {
                     this.personas = response.data
-                )
+                    this.initSecondMap();
+            	}
             ) 
         },
         save(evt) {
@@ -114,6 +119,9 @@ var app = new Vue({
               position: latLng,
               draggable: true
           });
+          
+          this.latitude = latLng.lat();
+          this.longitude = latLng.lng();
                     
           let that = this;
           marker.addListener('dragend', function() {
@@ -123,6 +131,94 @@ var app = new Vue({
               map.setCenter(pos);
             });
           this.markersArray.push(marker);
-        }
+        },
+        onClick(e) {
+            if (e.target.classList.contains('info-btn')) {
+            	
+            	var id = e.target.id;
+            	var marker = this.markersSecondMapArray.filter(v => v.getTitle() == id)[0];
+          	    marker.clickable = false;
+            	var infoWindow = this.markersInfoWindows.filter(c => c.Id == id)[0].InfoWindow;
+            	infoWindow.close();
+        		var pos = marker.getPosition();
+          	    
+            	if (this.currentClicked != null) {
+            		
+            		 var request = {
+            				    origin: new google.maps.LatLng(this.currentClicked.Latitude, this.currentClicked.Longitude),
+            				    destination: new google.maps.LatLng(pos.lat(), pos.lng()),
+            				    travelMode: 'DRIVING'
+            				  };
+            		 
+            		 let that = this;
+            		 
+            				  this.directionsService.route(request, function(result, status) {
+            				    if (status == 'OK') {
+            				      that.directionsRenderer.setDirections(result);
+            				      marker.setAnimation(null);
+            				    }
+            				  });
+            		this.currentClicked = null;
+            	} else {
+            		this.currentClicked =  { "Latitude": pos.lat(), "Longitude": pos.lng() };
+                	for(x = 0; x < this.markersSecondMapArray.length; x++) {
+                		if (this.markersSecondMapArray[x].getTitle() != id) {
+                			this.markersSecondMapArray[x].setAnimation(google.maps.Animation.BOUNCE);
+                		}
+                	} 
+                	alert("Elige otro marcador a donde ir");
+            	}
+            }
+          },
+        initSecondMap() {
+        	 secondMap = new google.maps.Map(document.getElementById('map2'), {
+                 center: {lat: 18.473, lng: -69.913},
+                 zoom: 14
+               });
+        	 
+        	 this.directionsService = new google.maps.DirectionsService();
+        	 this.directionsRenderer = new google.maps.DirectionsRenderer();
+        	 
+        	 this.directionsRenderer.setMap(secondMap);
+               
+               let that = this;
+               
+               for (x=0; x < this.personas.length; x++) {
+            	   if (this.personas[x].Latitude != "") {
+            		
+            		             		   
+	            	   let marker = new google.maps.Marker({
+		                   map: secondMap,
+		                   position: new google.maps.LatLng(this.personas[x].Latitude, this.personas[x].Longitude),
+		                   title: this.personas[x].Nombre
+		               });
+	            	   
+	            	   this.markersSecondMapArray.push(marker);
+	                         
+		               marker.addListener('click', function() {
+		                   let pos = marker.getPosition();
+		                   that.latitude = pos.lat();
+		                   that.longitude = pos.lng();
+		                   
+		                   var contentString = '<div id="content">'+
+	            		      marker.getTitle() +
+	            		      '<br/>' +
+	            		      '<br/>' +
+	            		      '<button class="btn btn-primary info-btn" id="'+marker.getTitle()+'">Ir</button>' +
+	            		      '</div>';
+
+		                   
+		            		  var infowindow = new google.maps.InfoWindow({
+		            		    content: contentString
+		            		  });
+		            		  
+		            		  that.markersInfoWindows.push({ "Id": marker.getTitle(), "InfoWindow": infowindow })
+		                   
+		                   infowindow.open(secondMap, marker);
+		                 });
+		               }
+               }
+        },
+        
     }
 });
